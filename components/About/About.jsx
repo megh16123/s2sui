3d15d5d5b2b3790713b57b477f0aa3320f53f0a5
Hardik Bhanot <76391935+Hardikbhanot@users.noreply.github.com>
2022-06-10 23:53:54 +0530

    Add files via upload

---
+++ components/About/About.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styles from './About.module.scss'
+import Image from 'next/image'
+import Navbar from '../LayoutComponents/Navbar'
+import Footer from '../LayoutComponents/footer'
+
+function About() {
+  return (
+    <>
+      <div className={styles.about_component}>
+    
+    <div className={styles.about}>
+      <Navbar/>
+        <div className={styles.about_components_parent}>
+        <div className={styles.left_component}>
+            <h1>About <br /> Us</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil deleniti esse accusantium. Natus enim maxime itaque unde tenetur, dolorem facere?</p>
+            
+        </div>
+        <div className={styles.right_component}>
+        <Image className='lightbulb' src="/logo.png" width="500px" height="500px" alt="lightBulb"></Image>
+        </div>
+    </div>
+    </div>
+    </div>
+    <div className={styles.founder}>
+    <svg className={styles.svg} viewBox="0 0 500 150" preserveAspectRatio="none" ><path className={styles.path} d="M251.92,-17.26 C209.59,70.55 285.77,81.41 251.92,151.48 L500.00,150.00 L500.00,0.00 Z" ></path></svg>
+        <div className={styles.founder_text}>
+        <h1>Our Founder</h1>
+        </div>
+        <div className={styles.founder_content}>
+        <div className={styles.left_container}>
+        <Image className='lightbulb' src="/logo.png" width="500px" height="500px" alt="lightBulb"></Image>
+        </div>
+        <div className={styles.right_container}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit incidunt repellat explicabo culpa labore consectetur odio, totam aliquid? Quod at magnam alias reprehenderit sequi magni, voluptatibus aliquam nobis autem ullam suscipit commodi explicabo velit placeat amet ab, optio dicta. Corrupti aliquam quis voluptate explicabo incidunt?
+        </div>
+        </div>
+    </div>
+
+    <div className={styles.our_aim}>
+      <div className={styles.heading}>
+        <h2>Our Aim</h2>
+        </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nisi accusamus veritatis itaque labore repellat dolores nihil quidem at perspiciatis, ad earum ratione dolor unde et praesentium velit numquam corporis libero. Consectetur eum debitis quos doloribus necessitatibus quaerat ipsam dolor, hic quis aliquid esse, earum ab sint inventore? Perferendis corporis numquam consequuntur quasi quod iusto tempore exercitationem, facilis quo eos nam in laborum dicta necessitatibus autem unde quaerat suscipit sunt distinctio doloremque itaque aut, molestiae iste alias. Saepe illum iure eum ex odio corrupti nihil! Dolore, iste. Facere at provident nam molestiae? Praesentium ad adipisci tempore molestias quisquam illo recusandae.</p>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default About
