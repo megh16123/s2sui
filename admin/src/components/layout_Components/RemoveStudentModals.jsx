2c0597160fd126ffda49427f0a045bc89f826217
Meghansh Tyagi <75660234+megh16123@users.noreply.github.com>
2022-06-16 22:58:00 +0530

    Update RemoveStudentModals.jsx

--- admin/src/components/layout_Components/RemoveStudentModals.jsx
+++ admin/src/components/layout_Components/RemoveStudentModals.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-
-function RemoveStudent() {
+import axios from "axios";
+function RemoveStudent({email}) {
     // For Modal 1
     const [show, setShow] = useState(false);
-
+    const [student,setStudent] = useState({});
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
@@ -14,7 +14,30 @@ function RemoveStudent() {
 
     const handleClose2 = () => setShow2(false);
     const handleShow2 = () => setShow2(true);
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios.post('http://localhost:3001/student/getbymail',{email:email});
+                setStudent(result.data);
+            } catch (error) {
+                setStudent({});
+            }
+        };
+    
+        fetchData();
+    }, [email]);
 
+    const handleRemove = async () => {
+        try {
+            const result = await axios.post('http://localhost:3001/student/deleteadmin',{email:email});
+            if(result.status === 200){
+            handleShow2();
+            handleClose();
+            }
+        } catch (error) {
+           handleClose();
+        }
+    }
 
     return (
         <>
@@ -32,23 +55,26 @@ function RemoveStudent() {
                 <Modal.Header closeButton>
                     <Modal.Title>Remove a student</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    SHOW STUDENT INFO HERE
+                <Modal.Body>     
+             {/* Name : {student.name} */}
+          <br />
+          Email : {student.email}
+          <br />
+          Fee Due : {student.feeDue}
                     <br /><br />
                     <span className="caution-message">NOTE: Once you remove the student, all the information of the student
                         will be deleted the database
                     </span>        
                 </Modal.Body>
                 <Modal.Footer>
-                Do you want to remove &lt; name of student &gt; ? 
+                Do you want to remove &lt;{student.name} &gt; ? 
                     <Button variant="outline-dark" onClick={handleClose}>
                         No
                     </Button>
                     <Button
                         variant="dark"
                         onClick={() => {
-                            handleShow2();
-                            handleClose();
+                          handleRemove();
                         }}
                     >
                         Yes
@@ -69,7 +95,7 @@ function RemoveStudent() {
                     <Modal.Title>Student Removed</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                &lt; Name of student &gt; has been removed
+                &lt; {student.name} &gt; has been removed
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="dark" onClick={handleClose2}>
