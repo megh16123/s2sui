5c53465c654d767c134a2e6049ff6b68b38c7d3b
dhannjayyy <d.dhannjayyy@gmail.com>
2022-07-21 13:52:50 +0530

    Responsivess to home page

--- ui/src/components/LayoutComponents/Navbar.jsx
+++ ui/src/components/LayoutComponents/Navbar.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.scss'
 import { HashLink } from 'react-router-hash-link';
+import { useNavigate } from 'react-router-dom';
 
 // import Image from 'next/image'
 function Navbar() {
@@ -9,7 +10,13 @@ function Navbar() {
   const [width, setWidth] = React.useState(0);
   React.useEffect(() => {
     setWidth(window.innerWidth);
-  });
+  },[]);
+
+  const navigate = useNavigate();
+  const redirectHome = () =>{
+    navigate('/');
+  }
+
   const whiteBurger={
     backgroundColor:"white"
   }
@@ -29,6 +36,7 @@ function Navbar() {
             zIndex: "100",
             height: "105vh"
   }
+
   const notShow={
     display: "none",
     width: "100%"
@@ -39,14 +47,14 @@ function Navbar() {
   
   return (
     <navbar className={styles.navbar} >
-        <div className={styles.logo}>
+        <div className={styles.logo} onClick={redirectHome}>
             <img src="/logo.png" alt="logo" width={70} height={70}></img>
         </div>
-        <ul className={styles.navlinks} style={width<1024?showlinks?show:notShow:nothing}>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/classes'>Classes</NavLink> </li>
-        <li><NavLink to='/about'>About Us</NavLink></li>
-        <li><HashLink smooth to='#footer'>Contact Us</HashLink></li>
+        <ul className={styles.navlinks} style={width<768?showlinks?show:notShow:nothing}>
+        <li className= {styles.navigationLinks}><NavLink to='/'>Home</NavLink></li>
+        <li className= {styles.navigationLinks}><NavLink to='/classes'>Classes</NavLink> </li>
+        <li className= {styles.navigationLinks}><NavLink to='/about'>About Us</NavLink></li>
+        <li className= {styles.navigationLinks}><HashLink smooth to='#footer'>Contact Us</HashLink></li>
         </ul>
         <div className={styles.burger} onClick={()=>setShowLinks(!showlinks)}>
           <div className={styles.line} style={showlinks?whiteBurger:blackBurger}></div>
--- ui/src/components/LayoutComponents/footer.jsx
+++ ui/src/components/LayoutComponents/footer.jsx
@@ -1,79 +1,79 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 import Modal from '../pages/misc/Modal'
 import ContactFrom from '../pages/misc/ContactFrom';
 import styles from './footer.module.scss'
 
 function Footer() {
-  const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-  const displayModal = () => {
-      setShowModal(true); 
-  }
+    const displayModal = () => {
+        setShowModal(true);
+    }
 
-  const closeModal = () => {
-      setShowModal(false);
-  }
+    const closeModal = () => {
+        setShowModal(false);
+    }
 
-  return (
-<>
-<hr/>
-    <section className={styles.seventh}>
-    <div id="footer" className={styles.upper_div}>
-      <div className={styles.left}>
-        Are You Ready To Start Your Course Now
-      </div>
-      <div className={styles.right}>
-      <Modal title='Contact Us' buttonText='Contact Us' showModal={showModal} displayModal={displayModal} closeModal={closeModal} cssClass="contactForm">{<ContactFrom/>}</Modal>
-      </div>
-    </div>
+    return (
+        <>
+            <section className={styles.seventh}>
+            {/* <hr /> */}
+                <div id="footer" className={styles.upper_div}>
+                    <div className={styles.left}>
+                        Are You Ready To Start Your Course Now
+                    </div>
+                    <div className={styles.right}>
+                        <Modal title='Contact Us' buttonText='Contact Us' showModal={showModal} displayModal={displayModal} closeModal={closeModal} cssClass="contactForm">{<ContactFrom />}</Modal>
+                    </div>
+                </div>
 
-    <footer className={styles.footer}>
-        <div className={styles.products}>
-            <h2>Products</h2>
-            <ul>
-                <li>Online Tutor</li>
-                <li>Private Teacher</li>
-                <li>E Books</li>
-                <li>premium E Course</li>
-                <li>Our Blogs</li>
-            </ul>
-        </div>
-        <div className={styles.information}>
-        <h2>Information</h2>
-            <ul>
-                <li>Online Tutor</li>
-                <li>Private Teacher</li>
-                <li>E Books</li>
-                <li>premium E Course</li>
-                <li>Our Blogs</li>
-            </ul>
-        </div>
-        <div className={styles.company}>
-        <h2>Company</h2>
-            <ul>
-                <li>Online Tutor</li>
-                <li>Private Teacher</li>
-                <li>E Books</li>
-                <li>premium E Course</li>
-                <li>Our Blogs</li>
-            </ul>
-        </div>
-        <div className={styles.getintouch}>
-        <h2>Get in touch</h2>
-            
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero excepturi error delectus explicabo! Hic ea expedita animi id corporis quis quam repellendus, voluptatem fugiat, in quisquam perferendis est alias.</p>
-            <ul className={styles.icons}>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
-    </footer>
-    
-</section>
-</> 
-  )
+                <footer className={styles.footer}>
+                    <div className={styles.products}>
+                        <h2>Products</h2>
+                        <ul>
+                            <li>Online Tutor</li>
+                            <li>Private Teacher</li>
+                            <li>E Books</li>
+                            <li>premium E Course</li>
+                            <li>Our Blogs</li>
+                        </ul>
+                    </div>
+                    <div className={styles.information}>
+                        <h2>Information</h2>
+                        <ul>
+                            <li>Online Tutor</li>
+                            <li>Private Teacher</li>
+                            <li>E Books</li>
+                            <li>premium E Course</li>
+                            <li>Our Blogs</li>
+                        </ul>
+                    </div>
+                    <div className={styles.company}>
+                        <h2>Company</h2>
+                        <ul>
+                            <li>Online Tutor</li>
+                            <li>Private Teacher</li>
+                            <li>E Books</li>
+                            <li>premium E Course</li>
+                            <li>Our Blogs</li>
+                        </ul>
+                    </div>
+                    <div className={styles.getintouch}>
+                        <h2>Get in touch</h2>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero excepturi error delectus explicabo! Hic ea expedita animi id corporis quis quam repellendus, voluptatem fugiat, in quisquam perferendis est alias.</p>
+                        <ul className={styles.icons}>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </footer>
+
+            </section>
+        </>
+    )
 }
 
 export default Footer
