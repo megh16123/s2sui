c590638a8e1f0a87e67e30caa1ec21ee30d2b1ac
Meghansh Tyagi <75660234+megh16123@users.noreply.github.com>
2022-07-31 21:24:00 +0530

    Update ContactFrom.jsx

--- ui/src/components/pages/misc/ContactFrom.jsx
+++ ui/src/components/pages/misc/ContactFrom.jsx
@@ -1,26 +1,47 @@
 import React from 'react'
 import './ContactFrom.scss'
+import { useState,useCallback } from 'react'
+import axios from 'axios'
 
-const ContactFrom = () => {
+const ContactFrom = ({closeModal}) => {
+    const [contact,setContact] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+    const onInputChange= useCallback(
+
+        (ev)=>{
+          const name = ev.target.name;
+          const value = ev.target.value;
+            setContact({...contact, [name]:value})
+        }
+      ,[contact])
+      const onSubmit = async()=>{
+     const result = await axios.post('http://localhost:3001/contactus',contact)
+        if(result.status===200){
+            closeModal();
+        }
+    }
     return (
         <>
-            <form action='' method='post' className='contactForm'>
+            <form onSubmit={(e)=>{e.preventDefault()}} method='post' className='contactForm'>
             <div className='upperSection'>
 
                 <div className='inputContainers'>
                     <label htmlFor="nameInput">Name</label>
-                    <input id="nameInput" type="text" name="name" placeholder='Name' />
+                    <input id="nameInput" type="text" name="name" onChange={onInputChange}  placeholder='Name' />
                 </div>
                 <div className='inputContainers'>
                     <label htmlFor="emailInput">Email</label>
-                    <input id="emailInput" type="email" name="email" placeholder='Email' />
+                    <input id="emailInput" type="email" name="email" onChange={onInputChange} placeholder='Email' />
                 </div>
                 <div className='inputContainers'>
                     <label htmlFor="messageInput">Message</label>
-                    <textarea row={5} id="messageInput" type="text" name="name" placeholder='Message' />
+                    <textarea row={5} id="messageInput" type="text" name="message" onChange={onInputChange} placeholder='Message' />
                 </div>
             </div>
-                <button type='submit' className='submitButton'>Submit!!!</button>
+                <button type='submit' className='submitButton' onClick={onSubmit}>Submit!!!</button>
             </form>
         </>
     )
