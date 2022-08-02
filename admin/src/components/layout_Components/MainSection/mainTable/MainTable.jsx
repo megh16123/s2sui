14ff8a4123ee6ecd58e45e31768a23bfb8dbc07e
dhannjayyy <d.dhannjayyy@gmail.com>
2022-08-02 23:34:49 +0530

    sdfkjb

--- admin/src/components/layout_Components/MainSection/mainTable/MainTable.jsx
+++ admin/src/components/layout_Components/MainSection/mainTable/MainTable.jsx
@@ -1,9 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import EditInfoModals from "../../EditInfoModals";
 import MoreInfo from "../../MoreInfoModals";
 import RemoveModals from "../../RemoveModals";
 
+// export function TblFunc(props) {
+ 
+//     $.DataTable = require('datatables.net')
+//     const tableRef = useRef()
+     
+//     useEffect(() => {
+//         console.log(tableRef.current)
+//         const table = $(tableRef.current).DataTable(
+//             {
+//                 data: props.data,
+//                     columns: [
+//                         { title: "Name"},
+//                         { title: "Position"},
+//                         { title: "Office"},
+//                         { title: "Extn."},
+//                         { title: "Start data"},
+//                         { title: "Salary"}
+//                     ],
+//                     destroy: true  // I think some clean up is happening here
+//             }
+//         )
+//         // Extra step to do extra clean-up.
+//         return function() {
+//             console.log("Table destroyed")
+//             table.destroy()
+//         }
+//     },[])
+//         return (
+//             <div>
+//                 <table className="display" width="100%" ref={ tableRef }></table>
+//             </div>
+             
+//         )
+//     }
+
 function MainTable(props) {
+
     const style = {
         backgroundColor: "#cccccc !important"
     };
@@ -31,6 +67,9 @@ function MainTable(props) {
                 })
                 setData(d);
             }
+            $(document).ready(function loadDataTble(){
+                $("#dataTable").DataTable();
+              });
         }
         fetchData();
 
@@ -80,7 +119,7 @@ function MainTable(props) {
                                     <td>{null}</td>
                                     <td>{null}</td>
                                 </tr>
-                                {datafilt()}
+                                {/* {datafilt()} */}
                             </tbody>
                         </table>
                     </div>
