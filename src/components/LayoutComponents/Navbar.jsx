1d965db4de194513d589a7aa29de18b4949fe685
HardikBhanot <hardikbhanot123@gmail.com>
2022-08-31 12:10:18 +0530

    navlinks order changed

--- src/components/LayoutComponents/Navbar.jsx
+++ src/components/LayoutComponents/Navbar.jsx
@@ -37,7 +37,7 @@ function Navbar() {
             justifyContent: "space-around",
             alignItems: "center",
             zIndex: "100",
-            height: "100vh"
+            height: "99vh"
   }
 
   const notShow={
@@ -61,8 +61,8 @@ function Navbar() {
         <ul className={styles.navlinks} style={width<=768?showlinks?show:notShow:nothing}>
         <li className= {styles.navigationLinks}><NavLink to='/'>Home</NavLink></li>
         <li className= {styles.navigationLinks}><NavLink to='/classes'>Classes</NavLink> </li>
-        <li className= {styles.navigationLinks}><NavLink to='/about'>About Us</NavLink></li>
         <li className= {styles.navigationLinks}><HashLink to='#footer'>Contact Us</HashLink></li>
+        <li className= {styles.navigationLinks}><NavLink to='/about'>About Us</NavLink></li>
         </ul>
         <div className={styles.burger} onClick={()=>setShowLinks(!showlinks)} style={showlinks?fixed:notfixed}>
           <div className={styles.line} style={showlinks?whiteBurger:blackBurger}></div>
